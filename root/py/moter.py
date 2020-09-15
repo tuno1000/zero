94485dd3775043285ef3030d1fa224799ecfb02e
tuno1000 <tuno_m@yahoo.co.jp>
2020-09-15 13:02:26 +0900

    moter mod timer in

--- root/py/moter.py
+++ root/py/moter.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+import datetime
+
 #ＧＰＩＯのポートを設定
 R_VREF=13
 R_IN1=19
@@ -13,8 +15,16 @@ GPIO.setup([R_VREF,R_IN1,R_IN2],GPIO.OUT)
 #回転速度を指定
 pwm_r = GPIO.PWM(R_VREF,100)
 pwm_r.start(100)
+
+loopTime = 1
+
+endTime = datetime.datetime.now() + datetime.timedelta(minutes=loopTime)
+
 try:
     while True:
+        
+        if datetime.datetime.now() > endTime:
+            break
         print("start")
         #モーターを停止する
         GPIO.output(R_IN1,GPIO.LOW)
@@ -24,7 +34,7 @@ try:
         #時計回りにモーターを回す
         GPIO.output(R_IN1,GPIO.HIGH)
         GPIO.output(R_IN2,GPIO.LOW)
-        sleep(10)
+        sleep(2)
         print("start")
         #モーターを停止する
         GPIO.output(R_IN1,GPIO.LOW)
@@ -34,7 +44,7 @@ try:
         #時計回りにモーターを回す
         GPIO.output(R_IN1,GPIO.LOW)
         GPIO.output(R_IN2,GPIO.HIGH)
-        sleep(10)
+        sleep(2)
 except KeyboardInterrupt:
     pass
 
